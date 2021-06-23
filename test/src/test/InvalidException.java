047ee6ebde52b5978904c63af5022066dd2d4328
Cloud_Forest <61158187+trongtuanit@users.noreply.github.com>
2021-06-24 01:13:14 +0700

    demo custom exception in java core

---
+++ test/src/test/InvalidException.java
@@ -0,0 +1,13 @@
+package test;
+
+public class InvalidException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidException(String message) {
+		super(message);
+	}
+}
+
---
+++ test/src/test/NotFoundException.java
@@ -0,0 +1,12 @@
+package test;
+
+public class NotFoundException extends RuntimeException{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NotFoundException(String message) {
+		super(message);
+	}
+}
