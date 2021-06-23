7a50fedbfcd5ef46205337723b5a7b9fcaaca9a7
Cloud_Forest <61158187+trongtuanit@users.noreply.github.com>
2021-06-24 01:15:35 +0700

    demo custom exception java core

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -4,28 +4,27 @@ package test;
 //import java.util.List;
 
 public class main {
-	
+
 	public static void main(String[] args) {
-		
-	    try {
-			if(!"hello".equals("what"))
+
+		try {
+			if (!"hello".equals("what"))
 				throw new InvalidException("Thong tin khong hop le!");
 			else
 				System.out.println("hello world");
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-	    
-	    
-	    try {
-			if(!"hello".contains("u"))
+
+		try {
+			if (!"hello".contains("u"))
 				throw new NotFoundException("Khong tim thay du lieu!");
 			else
 				System.out.println("hello world");
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-	    
+
 	}
 
 }
