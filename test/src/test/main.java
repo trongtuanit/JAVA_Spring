e61e86e1a7cedc1ecf32ef95ea6d765352b6eec2
Cloud_Forest <61158187+trongtuanit@users.noreply.github.com>
2021-06-24 10:51:37 +0700

    demo custom exceptions handlers java core

--- test/src/test/main.java
+++ test/src/test/main.java
@@ -1,30 +1,33 @@
 package test;
 
-//import java.util.ArrayList;
-//import java.util.List;
-
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
-		try {
-			if (!"hello".equals("what"))
-				throw new InvalidException("Thong tin khong hop le!");
-			else
-				System.out.println("hello world");
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-
-		try {
-			if (!"hello".contains("u"))
-				throw new NotFoundException("Khong tim thay du lieu!");
-			else
-				System.out.println("hello world");
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-
+//		try {
+//			if (!"hello".equals("what"))
+//				throw new InvalidException("Thong tin khong hop le!");
+//			else
+//				System.out.println("hello world");
+//		} catch (Exception e) {
+//			System.out.println(e);
+//		}
+//
+//		try {
+//			if (!"hello".contains("u"))
+//				throw new NotFoundException("Khong tim thay du lieu!");
+//			else
+//				System.out.println("hello world");
+//		} catch (Exception e) {
+//			System.out.println(e);
+//		}
+		
+		// Neu nhu khong co try - catch thi sau thi nem ra 1 loi chuong trinh se dung lai luon
+		if (!"hello".contains("u"))
+			throw new NotFoundException("Khong tim thay du lieu!");
+			
+		if (!"hello".equals("what"))
+			throw new InvalidException("Thong tin khong hop le!");
 	}
 
 }
